78f8c1fad7044dde8d691c3c2b79aeda11cafeef
wbarran <barran.william@gmail.com>
2026-02-25 10:38:27 +0100

    ajout de expr dans symbole

--- symbole.h
+++ symbole.h
@@ -3,27 +3,52 @@
 #include <string>
 using namespace std;
 
-enum Identificateurs { OPENPAR, CLOSEPAR, PLUS, MULT, INT, FIN, ERREUR };
-
-const string Etiquettes[] = { "OPENPAR", "CLOSEPAR", "PLUS", "MULT", "INT", "FIN", "ERREUR" };
-
-class Symbole {
-   public:
-      Symbole(int i) : ident(i) {  }
-      virtual ~Symbole() { }
-      operator int() const { return ident; }
-      virtual void Affiche();
-
-   protected:
-      int ident;
+enum Identificateurs
+{
+   OPENPAR,
+   CLOSEPAR,
+   PLUS,
+   MULT,
+   EXPR,
+   INT,
+   FIN,
+   ERREUR
 };
 
-class Entier : public Symbole {
-   public:
-      Entier(int v) : Symbole(INT), valeur(v) { }
-      ~Entier() { }
-      virtual void Affiche();
-   protected:
-      int valeur;
+const string Etiquettes[] = {"OPENPAR", "CLOSEPAR", "PLUS", "MULT", "EXPR"
+                                                                    "INT",
+                             "FIN", "ERREUR"};
+
+class Symbole
+{
+public:
+   Symbole(int i) : ident(i) {}
+   virtual ~Symbole() {}
+   operator int() const { return ident; }
+   virtual void Affiche();
+
+protected:
+   int ident;
 };
 
+class Entier : public Symbole
+{
+public:
+   Entier(int v) : Symbole(INT), valeur(v) {}
+   ~Entier() {}
+   virtual void Affiche();
+
+protected:
+   int valeur;
+};
+
+class Expr : public Symbole
+{
+public:
+   Expr(int v) : Symbole(EXPR), valeur(v) {}
+   int getValeur() const { return valeur; }
+   void Affiche() override { cout << valeur; }
+
+private:
+   int valeur;
+};
